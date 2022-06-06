6bc86df4b9a2a3cece942f95d914be772e40c378
TLongP <thlongph@gmail.com>
2022-06-06 20:07:17 +0200

    update for docker

--- preprocessing.py
+++ preprocessing.py
@@ -3,14 +3,20 @@ import json
 
 
 def load_labels(label_path):
+    """loading labels
+        label_path is a tensor"""
     with open(label_path.numpy(), 'r', encoding = "utf-8") as f:
         label = json.load(f)
     return [label['class']], label['bbox']
 def load_image(img_path):
+    """read image and return a tensor"""
     byte_img = tf.io.read_file(img_path)
     img = tf.io.decode_jpeg(byte_img)
     return img
-def combine_data_frompath(labels_path,images_path,shuffle_buffer=5000,batch_size=32):
+def combine_data_frompath(labels_path,images_path,shuffle_buffer=5000,batch_size=32,autotune=False):
+    """first we read the label and image then we zip them
+    do not shuffle the data before zip the data else the labels and images
+    do not math each other"""
     labels = tf.data.Dataset.list_files(labels_path,shuffle=False)
     labels = labels.map(lambda x: tf.py_function(func=load_labels,inp=[x],Tout=[tf.uint8,tf.float32]))
     images = tf.data.Dataset.list_files(images_path,shuffle=False)
@@ -19,4 +25,6 @@ def combine_data_frompath(labels_path,images_path,shuffle_buffer=5000,batch_size
     images = images.map(lambda x: x/255)
     data = tf.data.Dataset.zip((images,labels))
     data = data.shuffle(shuffle_buffer)
-    return data.batch(batch_size=batch_size).cache().prefetch(tf.data.AUTOTUNE)
+    if autotune:
+        return data.batch(batch_size=batch_size).cache().prefetch(tf.data.AUTOTUNE)
+    return data.batch(batch_size=batch_size).cache().prefetch(1)
